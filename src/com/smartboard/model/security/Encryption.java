5ab135f70fb55dcbb9033efb3b7a99f09979111d
Carl. K <48504139+carlkarama@users.noreply.github.com>
2021-10-25 21:54:53 +1100

    Delete src/com/smartboard directory

--- src/com/smartboard/model/security/Encryption.java
+++
@@ -1,9 +0,0 @@
-package com.smartboard.model.security;
-
-/** @author Carl Karama
- *  @version 1.0
- *  */
-
-public class Encryption {
-
-}
--- src/com/smartboard/model/workspace/MoveTaskToNewColumn.java
+++
@@ -1,7 +0,0 @@
-package com.smartboard.model.workspace;
-
-import com.smartboard.model.workspace.Columnable;
-
-public interface MoveTaskToNewColumn extends Columnable {
-    public void moveTaskToNewColumn();
-}
